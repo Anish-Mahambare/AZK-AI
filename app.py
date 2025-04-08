fc3b696f7a666bc0db29be1bd76a17bacb7fcc9a
Anish Mahambare <anishmahambare@gmail.com>
2025-04-07 21:35:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,26 +5,25 @@ import numpy as np
 import io
 from torchvision import transforms
 
-# Initialize Flask app
 app = Flask(__name__)
 
-# Load ONNX model
-onnx_path = 'melanoma_classifier_50.onnx'  # Path to your ONNX model
+
+onnx_path = 'melanoma_classifier_50.onnx'
 session = ort.InferenceSession(onnx_path)
 
-# Image transformation for the model
+
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.Normalize([0.5], [0.5])  # Same normalization as before
+    transforms.Normalize([0.5], [0.5])  
 ])
 
-# Route to display the homepage
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Route to handle image upload and prediction
+
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'file' not in request.files:
@@ -34,16 +33,16 @@ def predict():
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
 
-    # Read image file
-    image = Image.open(file.stream).convert('RGB')
-    image = transform(image).unsqueeze(0).numpy()  # Preprocess and convert to numpy array
 
-    # Run the model on the image
+    image = Image.open(file.stream).convert('RGB')
+    image = transform(image).unsqueeze(0).numpy()  
+
+  
     inputs = {session.get_inputs()[0].name: image}
     outputs = session.run(None, inputs)
     prediction = np.argmax(outputs[0], axis=1)[0]
 
-    # Return prediction
+   
     if prediction == 0:
         result = 'Benign'
     else:
